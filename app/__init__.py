3ff8d1864a8abe6f1de583f2d1918b12ff315250
Damian Ptaszkiewicz <kewinptaszkiewicz@gmail.com>
2025-04-05 01:33:00 +0200

    Implement contact form email sending

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from config import Config
+from flask_mail import Mail
+
+mail = Mail()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
+    mail.init_app(app)
     with app.app_context():
         from app import routes
 
--- config.py
+++ config.py
@@ -4,3 +4,10 @@ from dotenv import load_dotenv
 load_dotenv()
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY") or 'super_secret_key'
+
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = os.getenv('MAIL_USERNAME')
